922fba3a7cf6ea9750b6eb4a4c0f448f2ccef5b8
Yosuke Hara <yousuke.hara@gmail.com>
2014-02-25 17:31:18 +0900

    Modify children of the sup

--- src/savanna_agent_sup.erl
+++ src/savanna_agent_sup.erl
@@ -45,12 +45,12 @@ start_link() ->
 %% ===================================================================
 init([]) ->
     Children = [
-                {folsom,
-                 {folsom_sup, start_link, []},
+                {savanna_commons_sup,
+                 {savanna_commons_sup, start_link, []},
                  permanent,
                  2000,
                  supervisor,
-                 [folsom]},
+                 [savanna_commons_sup]},
 
                 {savanna_agent_worker,
                  {savanna_agent_worker, start_link,
