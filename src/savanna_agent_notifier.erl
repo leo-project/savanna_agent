662ef459680dbc1141749087de415902b887fc84
Yosuke Hara <yousuke.hara@gmail.com>
2014-04-22 16:30:38 +0900

    Refactor notify-fun

--- src/savanna_agent_notifier.erl
+++ src/savanna_agent_notifier.erl
@@ -42,46 +42,46 @@ notify(#sv_result{metric_group_name = MetricGroup,
                   adjusted_step = DateTime,
                   col_name = Key,
                   result = Value}) ->
-    notify(MetricGroup, DateTime, {Key, Value}, 1).
+    notify(DateTime, MetricGroup, Key, Value, 1).
+%% notify(MetricGroup, DateTime, Key, Value, 1).
 
 %% @private
-notify(_MetricGroup, _DateTime, _KeyAndVal, ?DEF_MAX_FAIL_COUNT) ->
+notify(_DateTime,_MetricGroup,_Key,_Val, ?DEF_MAX_FAIL_COUNT) ->
     %% @TODO enqueue a fail message
     ok;
-notify(MetricGroup, DateTime, {Key, Value}, Times) ->
-    %% Retrieve the destination node(s)
-    Node = case savanna_agent_tbl_members:find_by_state('running') of
-               {ok, Members} ->
-                   Len = length(Members),
-                   lists:nth(erlang:phash2(leo_date:now(), Len) + 1, Members);
-               _ ->
-                   []
-           end,
-
-    %% Transfer calculated statistics/metrics
-    case Node of
-        [] ->
-            notify(MetricGroup, DateTime, {Key, Value}, ?DEF_MAX_FAIL_COUNT);
-        _ ->
-            case notify_1(Node, MetricGroup, DateTime, Key, Value) of
+notify(DateTime, MetricGroup, Key, Val, Times) ->
+    %% Retrieve destination node(s)
+    case savanna_agent_tbl_members:find_by_state('running') of
+        {ok, Members} ->
+            %% Notify a message to a destination node
+            Len  = length(Members),
+            Node = lists:nth(erlang:phash2(
+                               leo_date:now(), Len) + 1, Members),
+            case notify_1(Node, MetricGroup, DateTime, Key, Val) of
                 ok ->
                     ok;
                 _ ->
-                    notify(MetricGroup, DateTime, {Key, Value}, Times + 1)
-            end
+                    notify(DateTime, MetricGroup,
+                           Key, Val, Times + 1)
+            end;
+        _ ->
+            notify(DateTime, MetricGroup,
+                   Key, Val, ?DEF_MAX_FAIL_COUNT)
     end.
 
+
 %% @private
-notify_1(Node, MetricGroup, DateTime, Key, Value) ->
+notify_1(Node, DateTime, MetricGroup, Key, Val) ->
     case svc_tbl_metric_group:get(MetricGroup) of
         {ok, #sv_metric_group{schema_name = Schema}} ->
             case leo_rpc:call(Node, savannadb_api, notify,
-                              [Schema, MetricGroup, DateTime, {Key, Value}]) of
+                              [DateTime, Schema,
+                               MetricGroup, Key, Val]) of
                 ok ->
                     ok;
                 _ ->
-                    {error, ?ERROR_COULD_NOT_GET_SCHEMA}
+                    {error, ?ERROR_COULD_NOT_TRANSFER_MSG}
             end;
         _ ->
-            {error, ?ERROR_COULD_NOT_TRANSFER_MSG}
+            {error, ?ERROR_COULD_NOT_GET_SCHEMA}
     end.
